44c71a94a25bbb4b80f27a1126c2dbe4f7f1ac24
alan94025 <alan0307221@gmail.com>
2024-11-03 18:51:15 -0700

    Update LilME_tb.v

--- LilME_tb.v
+++ LilME_tb.v
@@ -11,11 +11,13 @@ module LilME_tb;
     reg [2:0] ME_opcode;
     reg A_opcode;
     reg B_opcode;
-    reg [aw:0] Address_out;
+    wire [aw:0] Address_out;
     reg [dw:0] Data_in;
     wire Busy;
     wire [dw:0] Data_out;
     wire [dw:0] result;
+
+    integer i;
 
    LilME #(
         .dw(dw),
@@ -44,38 +46,67 @@ initial begin
         A_opcode = 0;
         B_opcode = 0;
         Data_in = 32'h00000000;
-        Address_out = 32'h00000000;
+	//Address_out = 32'h00000000;
+
 
         #10 reset = 0;
 
-//testing Load_address （ME_opcode = 3'b001）
+//testing Load_address \uff08ME_opcode = 3'b001\uff09
 
         #10 ME_opcode = 3'b001;
         #10 ME_opcode = 3'b000; //IDLE
 
 
-//testing Load_Matrix_a （ME_opcode = 3'b010）
+//testing Load_Matrix_a \uff08ME_opcode = 3'b010\uff09
+	//for(i=0;i < row * col;i=i+1) begin
+        //#10 ME_opcode = 3'b010;
+        //A_opcode = 1;
+        //Data_in = i;
+        //#20 ME_opcode = 3'b000;
+	//end
+        //A_opcode = 0;
 
-        #10 ME_opcode = 3'b010;
-        A_opcode = 1;
-        Data_in = 32'hA5A5A5A5;
-        #20 ME_opcode = 3'b000;
-        A_opcode = 0;
+        ME_opcode = 3'b010; 
+        A_opcode = 1;       
+        for ( i = 0; i < row * col; i = i + 1) begin
+            Data_in = i;    
+            #10;            
 
-//testing Load_Matrix_b （ME_opcode = 3'b011）
+           
+            while (Busy) #10;
+        end
+        A_opcode = 0;      
+        ME_opcode = 3'b000; 
 
-        #10 ME_opcode = 3'b011;
-        B_opcode = 1;
-        Data_in = 32'h5A5A5A5A;
-        #20 ME_opcode = 3'b000;
-        B_opcode = 0;
 
- // testing MUL（ME_opcode = 3'b101）
+//testing Load_Matrix_b \uff08ME_opcode = 3'b011\uff09
+	//for(i=0;i < row * col;i=i+1) begin
+       // #10 ME_opcode = 3'b011;
+       // B_opcode = 1;
+       // Data_in = i+1;
+       // #20 ME_opcode = 3'b000;
+	//end
+       // B_opcode = 0;
+
+        ME_opcode = 3'b011; 
+        B_opcode = 1;       
+        for ( i = 0; i < row * col; i = i + 1) begin
+            Data_in = i + 1;  
+            #10;              
+
+           
+            while (Busy) #10;
+        end
+        B_opcode = 0;      
+        ME_opcode = 3'b000; 
+
+
+ // testing MUL\uff08ME_opcode = 3'b101\uff09
 
         #10 ME_opcode = 3'b101;
         #20 ME_opcode = 3'b000;
     
- // testing read_MUL（ME_opcode = 3'b111）
+ // testing read_MUL\uff08ME_opcode = 3'b111\uff09
 
         #10 ME_opcode = 3'b111;
         #50 ME_opcode = 3'b000;
